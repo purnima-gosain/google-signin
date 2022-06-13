55fe2bb5f08c4b8e13764c07a29b3cd324cbbdbb
purnima-poo <np03a190173@herladcollege.edu.np>
2022-06-13 13:05:42 +0545

    feat:create model

--- google_signin/lib/model/users_detail.dart
+++ google_signin/lib/model/users_detail.dart
@@ -4,5 +4,20 @@ class UserDetails {
   String? photoUrl;
 
   //constructors
+  UserDetails({this.displayName, this.email, this.photoUrl});
+  UserDetails.fromJson(Map<String, dynamic> json) {
+    displayName = json['displayName'];
+    photoUrl = json['photoUrl'];
+    email = json['email'];
+  }
 
+  Map<String, dynamic> toJson() {
+    //object-data
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['displayName'] = this.displayName;
+    data['photoUrl'] = this.photoUrl;
+    data['email'] = this.email;
+
+    return data;
+  }
 }
