ebb978415a6e40daa51c74bbbd46be51c6ff0740
purnima-poo <np03a190173@herladcollege.edu.np>
2022-06-13 13:18:38 +0545

    feat:added google-signin controller

--- google_signin/lib/controllers/google_signin_controller.dart
+++ google_signin/lib/controllers/google_signin_controller.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
+class GoogleSignInController with ChangeNotifier {
+//object
+
+  var _googleSignIn = GoogleSignIn();
+  GoogleSignInAccount? googleSignInAccount;
+
+  login() async {
+    this.googleSignInAccount = await _googleSignIn.signIn();
+
+    notifyListeners();
+  }
+
+  logout() async {
+    //empty value after logout
+
+    this.googleSignInAccount = await _googleSignIn.signOut();
+
+    notifyListeners();
+  }
+}
